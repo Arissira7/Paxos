a92528a6b7990a2837d1d56e65bbe2e71e47f0fe
Aris <147796666+Arissira7@users.noreply.github.com>
2026-07-26 14:15:31 +0800

    test file

--- go.mod
+++ go.mod
@@ -3,17 +3,21 @@ module github.com/Arissira7/paxos
 go 1.20
 
 require (
+	github.com/golang/protobuf v1.5.4
 	github.com/kr/pretty v0.3.1
+	github.com/stretchr/testify v1.11.1
 	golang.org/x/net v0.22.0
 	google.golang.org/grpc v1.64.0
 	google.golang.org/protobuf v1.33.0
 )
 
 require (
-	github.com/golang/protobuf v1.5.4 // indirect
+	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/kr/text v0.2.0 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/rogpeppe/go-internal v1.9.0 // indirect
 	golang.org/x/sys v0.18.0 // indirect
 	golang.org/x/text v0.14.0 // indirect
 	google.golang.org/genproto v0.0.0-20200526211855-cb27e3aa2013 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
---
+++ paxos/example_set_get_test.go
@@ -0,0 +1,76 @@
+package paxos
+
+import (
+	"fmt"
+)
+
+func Example_setAndGetByKeyVer() {
+
+	acceptorIds := []int64{0, 1, 2}
+
+	servers := ServeAcceptors(acceptorIds)
+	defer func() {
+		for _, s := range servers {
+			s.Stop()
+		}
+	}()
+
+	// set foo₀ = 5
+	{
+		prop := Proposer{
+			Id: &PaxosInstanceId{
+				Key: "foo",
+				Ver: 0,
+			},
+			Bal: &BallotNum{N: 0, ProposerId: 2},
+		}
+		v := prop.RunPaxos(acceptorIds, &Value{Vi64: 5})
+		fmt.Printf("written: %v;\n", v.Vi64)
+	}
+
+	// get foo₀
+	{
+		prop := Proposer{
+			Id: &PaxosInstanceId{
+				Key: "foo",
+				Ver: 0,
+			},
+			Bal: &BallotNum{N: 0, ProposerId: 2},
+		}
+		v := prop.RunPaxos(acceptorIds, nil)
+		fmt.Printf("read:    %v;\n", v.Vi64)
+	}
+
+	// set foo₁ = 6
+	{
+		prop := Proposer{
+			Id: &PaxosInstanceId{
+				Key: "foo",
+				Ver: 1,
+			},
+			Bal: &BallotNum{N: 0, ProposerId: 2},
+		}
+		v := prop.RunPaxos(acceptorIds, &Value{Vi64: 6})
+		fmt.Printf("written: %v;\n", v.Vi64)
+	}
+
+	// get foo₁
+	{
+		prop := Proposer{
+			Id: &PaxosInstanceId{
+				Key: "foo",
+				Ver: 1,
+			},
+			Bal: &BallotNum{N: 0, ProposerId: 2},
+		}
+		v := prop.RunPaxos(acceptorIds, nil)
+		fmt.Printf("read:    %v;\n", v.Vi64)
+	}
+
+	// Output:
+	// written: 5;
+	// read:    5;
+	// written: 6;
+	// read:    6;
+
+}
